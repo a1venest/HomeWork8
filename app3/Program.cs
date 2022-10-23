186e1ffa2e0571ef0c3ce932c99016cc5a0d8887
Aivenest <aivenest@gmail.com>
2022-10-23 23:12:46 +0500

    app3

---
+++ app3/Program.cs
@@ -0,0 +1,69 @@
+﻿int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetNumberValue(int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue + 1);
+}
+
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = GetNumberValue(minValue, maxValue);
+        }
+    }
+}
+
+void MultiplicationArray(int[,] arr1, int[,] arr2, int[,] arr3)
+{
+    for (int i = 0; i < arr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr1.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr2.GetLength(1); k++)
+            {
+                arr3[i, j] += arr1[i, k] * arr2[k, j];
+            }
+        }
+    }
+}
+
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] InitializateArray(int m, int n)
+{
+    return new int[m, n];
+}
+
+int m = GetNumberFromConsole("Введите размерность массива: m");
+int n = GetNumberFromConsole("Введите размерность массива: n");
+int[,] array = InitializateArray(m, n);
+int[,] array2 = InitializateArray(m, n);
+int[,] array3 = InitializateArray(m, n);
+int maxValue = GetNumberFromConsole("Введите максимальное допустимое число в массиве:");
+int minValue = GetNumberFromConsole("Введите минимальное допустимое число в массиве:");
+FillArray(array, minValue, maxValue);
+PrintArray(array);
+Console.WriteLine();
+FillArray(array2, minValue, maxValue);
+PrintArray(array2);
+Console.WriteLine();
+MultiplicationArray(array,array2,array3);
+PrintArray(array3);
